6dc8050096a6da6b8914afedf231e9388c0f061e
shubham-singh-cometchat <91067224+shubham-singh-cometchat@users.noreply.github.com>
2021-10-29 18:34:03 +0530

    Update Private.js

--- Client/src/Private.js
+++ Client/src/Private.js
@@ -1,6 +1,6 @@
 const data = {
-  appID: '19644985d0305be7',
-  region: 'us',
-  Auth_key: '262daab49b9319c65f4cbba1f88e50def942dda5',
+  appID: 'comet chat appId',
+  region: 'comet chat region',
+  Auth_key: 'comet chat auth key',
 };
 export default data;
